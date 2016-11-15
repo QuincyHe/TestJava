b40c831cab1b6f633d64b24bb9f023de643b51db
Quincy He <quincyhe2015@gmail.com>
2016-11-15 01:46:39 -0800

    ？？？

---
+++ HelloAgainJava/src/HelloAgain.java
@@ -0,0 +1,10 @@
+
+public class HelloAgain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("我是第二版的啊");
+		System.out.println("你管得着么");
+	}
+
+}
